f757c131b01623f834199693f1627b6611f0a4f7
Burak Öztürk <burakozturk90@gmail.com>
2013-09-30 22:10:51 +0300

    initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Translatr::Application.config.secret_key_base = 'f4a50f75207ed3e928ca1fa9d04257c0645d80d81a83ecd415293ba542fb491e1ef75a0c64a30fac8f9702a30b5975efa0856a0dfbf78afec3c31ecfbc1f08b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Translatr::Application.config.session_store :cookie_store, key: '_translatr_session'
