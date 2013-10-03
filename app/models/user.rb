4430cf08c940caec92c290e9a08adc5d13953bb3
Burak Öztürk <burakozturk90@gmail.com>
2013-10-03 22:11:54 +0300

    validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
-end
+
+  has_secure_password
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: {with:  VALID_EMAIL_REGEX }
+  validates :last_name, presence: true end
