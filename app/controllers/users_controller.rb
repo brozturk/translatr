4b756f8c7b35e80a9f6743cea2afb2f742fd925f
Burak Öztürk <burakozturk90@gmail.com>
2013-10-03 22:09:35 +0300

    fnsh users controller for signing up users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path, success: 'Üyeliğiniz başarılı bir şekilde yapıldı.'
+    else
+      redirect_to new_user_path, error: 'Girmiş olduğunuz bilgilerde hata var.Lütfen tekrar deneyin.'
+    end
+  end
+
+  def index
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :last_name, :email, :password, 
+                                 :password_confirmation)
   end
 end
