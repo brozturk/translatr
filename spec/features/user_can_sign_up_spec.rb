8b3fafb45b3e29fcda9ccec4dffede6d537f07cf
Burak Öztürk <burakozturk90@gmail.com>
2013-10-03 22:11:17 +0300

    add specs for the user model and feature specs

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,12 +4,40 @@ feature 'User can sign up' do
   scenario 'with valid information' do
     visit new_user_path
     expect{
-      fill_in 'İsim', with: 'Burak'
-      fill_in 'Soy İsim', with: 'Ozturk'
+      fill_in 'İsim', with: 'burak'
+      fill_in 'Soy İsim', with: 'ozturk'
       fill_in 'Mail Adresi', with: 'burak@burak.com'
       fill_in 'Şifre', with: '123456'
       fill_in 'Şifre Tekrar', with: '123456'
       click_button 'Beni Kaydet!'
     }.to change(User, :count).by(1)
+  end
+
+  scenario 'with invalid email' do  
+    visit new_user_path
+    expect{
+      fill_in 'İsim', with: 'burak'
+      fill_in 'Soy İsim', with: 'ozturk'
+      fill_in 'Mail Adresi', with: 'bloblow'
+      fill_in 'Şifre', with: '123456'
+      fill_in 'Şifre Tekrar', with: '123456'
+      click_button 'Beni Kaydet!'
+    }.to_not change(User, :count).by(1) 
+    expect(page).to have_content 'Girmiş olduğunuz bilgilerde hata var.Lütfen tekrar deneyin.'
+    expect(current_path).to eq new_user_path
+  end
+
+  scenario 'with empty name field' do 
+    visit new_user_path
+    expect{
+      fill_in 'İsim', with: ''
+      fill_in 'Soy İsim', with: 'ozturk'
+      fill_in 'Mail Adresi', with: 'bloblow'
+      fill_in 'Şifre', with: '123456'
+      fill_in 'Şifre Tekrar', with: '123456'
+      click_button 'Beni Kaydet!'
+    }.to_not change(User, :count).by(1)
+    expect(page).to have_content 'Girmiş olduğunuz bilgilerde hata var.Lütfen tekrar deneyin.'
+    expect(current_path).to eq new_user_path 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe User do
+it { should validate_presence_of(:name) } 
+it { should validate_presence_of(:last_name) }
+it { should validate_presence_of(:email) } 
+
 end
