17a41fbe9eeac2c50f49fc75620b94b211b70304
Burak Öztürk <burakozturk90@gmail.com>
2013-10-03 14:39:03 +0300

    fix typo

--- spec/features/user_can_sign_up.rb
+++ spec/features/user_can_sign_up.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'User can sign up' do 
   scenario 'with valid information' do
-    visit new_use_path
+    visit new_user_path
     expect{
       fill_in 'İsim', with: 'Burak'
       fill_in 'Soy İsim', with: 'Ozturk'
