ea08d76db70c32724b4e0043ce32bd6a539f056d
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 22:08:29 +0300

    renamed file to user_logs_in

---
+++ spec/features/user_logs_in_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'user logs in' do 
+  scenario 'with valid information' do 
+    create_user_and_login
+    expect(page).to have_content 'Başarılı bir şekilde giriş yapıldı!'
+    expect(current_path).to eq users_path(@user)
+  end
+
+  scenario 'with remember_me box checked' do 
+    visit root_path
+    fill_in_form 
+    check('Beni Hatırla!')
+    click_button 'Giriş'
+    expect(page).to have_content 'Başarılı bir şekilde giriş yapıldı!'
+    expect(current_path).to eq users_path(@user)
+  end
+
+  scenario 'with invalid information' do 
+    login_with_invalid_info
+    expect(page).to have_content 'Giriş sırasında sorun oluştu lütfen bilgilerin doğruluğunu kontrol et' 
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'user can click signup link and go to that page' do
+    visit root_path
+    click_link 'Kayıt ol!'
+    expect(page).to have_content 'Hesap Oluştur'
+    expect(current_path).to eq new_user_path
+  end
+end
