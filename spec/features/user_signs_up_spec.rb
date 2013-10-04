55944a329d46c4b66068b6f66e01a0e52e0be372
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 18:21:05 +0300

    finish the user login feature specs.

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 feature 'user signs up' do 
   scenario 'with valid information' do 
-    user = create(:user)
-    visit root_path
-    fill_in 'Mail Adresi', with: user.email 
-    fill_in 'Şifre', with: user.password
-    click_button 'Giriş'
+    create_user_and_login
     expect(page).to have_content 'Başarılı bir şekilde giriş yapıldı!'
-    expect(current_path).to eq users_path(user)
+    expect(current_path).to eq users_path(@user)
+  end
+
+  scenario 'with invalid information' do 
+    login_with_invalid_info
+    expect(page).to have_content 'Giriş sırasında sorun oluştu lütfen bilgilerin doğruluğunu kontrol et' 
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'user can click signup link and go to that page' do
+    visit root_path
+    click_link 'Kayıt ol!'
+    expect(page).to have_content 'Hesap Oluştur'
+    expect(current_path).to eq new_user_path
   end
 end
