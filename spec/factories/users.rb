9ebfcf00cae305a1b6f85dfb039a650616e8f327
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 13:23:07 +0300

    create the user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
-    name { Faker::Name.firs_name } 
+    name { Faker::Name.first_name } 
     last_name { Faker::Name.last_name } 
     email { Faker::Internet.email } 
     password '123456'
