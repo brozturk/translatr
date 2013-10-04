631b7cfd66f72cd22dd3a0e496ec387e0acd1624
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 13:24:04 +0300

    write the feature spec for users sign in

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 feature 'user signs up' do 
   scenario 'with valid information' do 
+    user = create(:user)
     visit root_path
-    
+    fill_in 'Mail Adresi', with: user.email 
+    fill_in 'Şifre', with: user.password
+    click_button 'Giriş'
+    expect(page).to have_content 'Başarılı bir şekilde giriş yapıldı!'
+    expect(current_path).to eq users_path(user)
   end
 end
