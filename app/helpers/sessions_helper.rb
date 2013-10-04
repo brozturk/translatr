ba4d937637ec3109925b460ea2fc95019dceeff7
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 22:07:02 +0300

    changed the sign_in method to support remember me functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,13 @@
 module SessionsHelper
   def sign_in(user)
-  remember_token = User.new_remember_token
-  cookies.permanent[:remember_token] = remember_token
-  user.update_attribute(:remember_token, User.encrypt(remember_token))
-  self.current_user = user
+    remember_token = User.new_remember_token
+    if params[:remember_me]
+      cookies.permanent[:remember_token] = remember_token
+    else 
+      cookies[:remember_token] = remember_token
+    end
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
   end
 
   def current_user=(user)
