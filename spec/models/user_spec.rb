b907f66a288b8093ae7e8f438f00f3f8420f5889
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 15:30:49 +0300

    create unit test for testing the remember_token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,7 @@ it { should validate_presence_of(:name) }
 it { should validate_presence_of(:last_name) }
 it { should validate_presence_of(:email) } 
 
+@user = FactoryGirl.create(:user) 
+its(:remember_token) { should_not be_blank} 
+
 end
