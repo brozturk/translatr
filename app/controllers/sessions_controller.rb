9e45ee0b860fa3d62e66b6e07fedd5cbba906afc
Burak Öztürk <burakozturk90@gmail.com>
2013-10-04 14:32:37 +0300

    create the sessions controller create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email]) 
+    if user && user.authenticate(params[:session][:password])
+      redirect_to users_path(user), success: 'Başarılı bir şekilde giriş yapıldı!'
+    else
+      redirect_to root_path, danger: 'Giriş sırasında sorun oluştu lütfen bilgilerin doğruluğunu kontrol et' 
+    end
   end
 
   def destroy
