523b0ca03cf9cd72e44ecc85df5f51051c472d4a
Burak Öztürk <burakozturk90@gmail.com>
2013-10-07 19:10:44 +0300

    delete the sets_time_of_answer spec for future implementation

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -1,14 +1,8 @@
 require 'spec_helper'
 
 describe Translation do
-  
+
   it { should validate_presence_of(:question) }
   it { should belong_to(:user) }
 
-  it 'sets the time of the answer' do 
-    translation = Translation.new(question: 'some thing that needs translating?')
-    translation.answer = 'some answer'
-    translation.set_time_of_answer
-    translation.time_of_answer.should_not be_blank
-  end
 end
