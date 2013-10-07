ffce35100236c050c9572caf9e7d15f97c3dd286
Burak Öztürk <burakozturk90@gmail.com>
2013-10-07 21:54:07 +0300

    change migration

--- db/migrate/20131007184538_change_default_booleean_in_users_table.rb
+++ db/migrate/20131007184538_change_default_booleean_in_users_table.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultBooleeanInUsersTable < ActiveRecord::Migration
   def change
-    change_column_default :user, :translator, :boolean, :default => false
+    change_column :users, :translator, :boolean, :default => false
   end
 end
