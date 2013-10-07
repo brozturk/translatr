7daef7099411be5babadb8476c04045781ddf51a
Burak Öztürk <burakozturk90@gmail.com>
2013-10-07 21:54:21 +0300

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007141934) do
+ActiveRecord::Schema.define(version: 20131007184538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20131007141934) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
-    t.boolean  "translator",      default: true
+    t.boolean  "translator",      default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
