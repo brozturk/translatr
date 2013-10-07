0ace23c406648df03b72f777a6842aeedc12d798
Burak Öztürk <burakozturk90@gmail.com>
2013-10-07 22:28:17 +0300

    add set_as_admin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def set_as_translator
+    self.translator = true
+  end
+
   private
 
   def create_remember_token
