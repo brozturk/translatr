028d1acefa3a23fc5bb3cfc334014f6a7ec8ae75
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 16:09:00 +0300

    change translation validations and relationships

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,6 +1,9 @@
 class Translation < ActiveRecord::Base
   belongs_to :user
+  belongs_to :text
 
-  validates :question, presence: true
+  validates :text, presence: true
+  validates :user_id, presence: true
+  validates :text_id, presence: true
 
 end
