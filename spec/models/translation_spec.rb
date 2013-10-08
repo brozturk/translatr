7e410a1fcd97ba1620002e823fc10b2763f8eebc
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 16:04:24 +0300

    change unit tests to reflect changes to the transltaions table

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Translation do
 
-  it { should validate_presence_of(:question) }
-  it { should belong_to(:user) }
-
+  it { should validate_presence_of(:title) } 
+  it { should validate_presence_of(:text) } 
+  it { should belong_to(:user) } 
+  it { should belong_to(:text) }
 end
