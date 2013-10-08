4dac092ca3282b48027aa4bcc6d45977058bedb0
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 17:38:02 +0300

    create validations and relationships for the Text model

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,2 +1,8 @@
 class Text < ActiveRecord::Base
+  belongs_to :user
+  has_one :translation
+
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :user_id, presence: true
 end
