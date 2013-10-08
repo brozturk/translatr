1e1bc134e34bf588be5b41481b2d50f44c835da3
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 16:55:34 +0300

    new unit tests for the translation model

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Translation do
 
-  it { should validate_presence_of(:title) } 
+  it { should validate_presence_of(:text_id) } 
+  it { should validate_presence_of(:user_id) } 
   it { should validate_presence_of(:text) } 
   it { should belong_to(:user) } 
   it { should belong_to(:text) }
