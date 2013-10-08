27e1819b60652665deddfdb5345619cbf163ae98
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 17:28:51 +0300

    create unit tests for the Text model

---
+++ spec/models/text_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Text do 
+  it {should belong_to (:user)}
+  it {should have_one(:translation)}
+  it {should validate_presence_of(:title)} 
+  it {should validate_presence_of(:text)} 
+  it {should validate_presence_of(:user_id)} 
+end
