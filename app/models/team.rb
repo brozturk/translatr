23f80aa0aba04c82196306abc00e3e53790e53ab
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 21:37:13 +0300

    validations and relationships for the Team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,7 @@
-class Team < ActiveModel::Base
+class Team < ActiveRecord::Base
+  has_and_belongs_to_many(:users)
+  has_many :texts, through: :users
+  has_many :translations, through: :users
+
+  validates :name, presence: true
 end
