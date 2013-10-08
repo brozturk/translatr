41c351355a28d737d44849a783e06f2d2374a2e6
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 17:37:20 +0300

    update the schema with new tests table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008124948) do
+ActiveRecord::Schema.define(version: 20131008141643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "texts", force: true do |t|
+    t.text    "text"
+    t.string  "title"
+    t.integer "user_id"
+  end
 
   create_table "translations", force: true do |t|
     t.datetime "time_of_answer"
