052a2cd9a10c7b74bdbabe25b59a8e0c100dfafc
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 18:49:02 +0300

    updated schema with the teams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008150023) do
+ActiveRecord::Schema.define(version: 20131008154639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: true do |t|
+    t.string  "name"
+    t.integer "leader_id"
+  end
 
   create_table "texts", force: true do |t|
     t.text    "text"
