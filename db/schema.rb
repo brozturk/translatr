76dda1dc597a798b6de623e942b57a984ec2faf5
Burak Öztürk <burakozturk90@gmail.com>
2013-10-08 16:02:05 +0300

    updated schema reflecting changes for the translations table changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007184538) do
+ActiveRecord::Schema.define(version: 20131008124948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "translations", force: true do |t|
-    t.string   "question"
-    t.string   "answer"
     t.datetime "time_of_answer"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "text"
+    t.integer  "text_id"
   end
 
   create_table "users", force: true do |t|
