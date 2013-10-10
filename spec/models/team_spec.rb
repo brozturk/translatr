0262549dd32e0dd492667f4c9faaea97b39d2ad9
Burak Öztürk <burakozturk90@gmail.com>
2013-10-10 17:26:31 +0300

    Tests for the user_teams association

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Team do 
-  it {should have_and_belong_to_many(:users)}
-  it {should validate_presence_of(:name)}
-  it {should have_many(:texts).through(:users)}
-  it {should have_many(:translations).through(:users)}
+  it { should have_many(:users).through(:user_teams) }
+  it { should validate_presence_of(:name) }
+  it { should have_many(:texts).through(:users) }
+  it { should have_many(:translations).through(:users) }
 end
