c71aa87da4261fb599e32b3116a340d6ea7f44ea
Burak Öztürk <burakozturk90@gmail.com>
2013-10-10 19:51:18 +0300

    create the user_teams spec

---
+++ spec/models/user_teams_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe UserTeam do 
+  before do
+    @user = create(:user)
+    @team = create(:team)
+    @user_team = create(:user_team, user_id: @user.id, team_id: @team.id)
+  end
+  subject { @user_team }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:team) } 
+
+  describe 'assigning a user to a team with user_id should have pending state' do
+    its(:state) { should eq 'pending' }
+  end
+end
+
