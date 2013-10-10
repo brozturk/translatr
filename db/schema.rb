f788b96fe3f22b93bfc8223dafa58f7566d91748
Burak Öztürk <burakozturk90@gmail.com>
2013-10-10 17:17:45 +0300

    add the user_teams table to the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010135250) do
+ActiveRecord::Schema.define(version: 20131010140440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20131010135250) do
     t.datetime "updated_at"
     t.text     "text"
     t.integer  "text_id"
+  end
+
+  create_table "user_teams", force: true do |t|
+    t.integer "user_id"
+    t.integer "team_id"
+    t.string  "state"
   end
 
   create_table "users", force: true do |t|
