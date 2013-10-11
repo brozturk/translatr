a90a4bb2e463d70f8e551b2007b949c5377c2a9f
Burak Öztürk <burakozturk90@gmail.com>
2013-10-11 17:46:06 +0300

    change formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = create (:user) }
-
+  before { @user = create(:user) }
   subject { @user } 
+
   it {should validate_presence_of(:name)} 
   it {should validate_presence_of(:last_name)}
   it {should validate_presence_of(:email)}
@@ -18,4 +18,5 @@ describe User do
   describe 'remember token' do 
     its(:remember_token) { should_not be_blank }  
   end
+
 end
