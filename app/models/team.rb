e0c36b00def969820ba56b17591e2999d465a2da
Burak Öztürk <burakozturk90@gmail.com>
2013-10-11 10:43:18 +0300

    modify the team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,13 @@
 class Team < ActiveRecord::Base
+  validates :name, presence: true
+  
   has_many :users, through: :user_teams
   has_many :user_teams
   has_many :texts, through: :users
   has_many :translations, through: :users
 
-  validates :name, presence: true
+
+
+
+  
 end
