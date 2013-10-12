4a31c691754976712d5dfa1bfab09bfa20d4dfa2
Burak Öztürk <burakozturk90@gmail.com>
2013-10-12 15:48:13 +0300

    feature for creating a group

---
+++ spec/features/user_creates_a_group_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'user creates a gorup', js:true do 
+  scenario 'from the create group link' do 
+    create_user_and_login
+    visit user_path(@user)
+    click_link 'Grup Oluştur'
+    fill_in 'Grup İsmi', with: 'blaloblawgrou'
+    expect(page).to have_content 'Grup Yönet'
+    expect(page).to have_content 'Gruba Ekle'
+    expect (page).to have_content 'Grup İşlemleri'
+  end
+end
+
