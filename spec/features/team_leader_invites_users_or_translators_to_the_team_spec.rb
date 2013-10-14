656d686f619fcdbfa2d925867af0236578e85141
Burak Öztürk <burakozturk90@gmail.com>
2013-10-14 18:41:22 +0300

    change the spec due to some difficulties with scoping

--- spec/features/team_leader_invites_users_or_translators_to_the_team_spec.rb
+++ spec/features/team_leader_invites_users_or_translators_to_the_team_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 feature 'team leader invites users or translators to the team' do 
   background do 
-    @user2 = create(:user)
+    @user2 = create(:user, translator: true)
     create_user_and_login
     visit user_path(@user)
     click_link 'Grup Oluştur'
     fill_in 'Grup İsmi', with: 'Decoy Group'
     click_button 'Grubu Kur' 
     click_link 'Gruba Ekle'
-    click_button 'Gruba Davet Et'
+    within('form#translator') do 
+      click_button 'Gruba Davet Et'
+    end
   end
 
   scenario 'they recieve a request to join the team' do 
