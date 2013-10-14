5290cb47260d6e6669d3c3e794b529091e031aa8
Burak Öztürk <burakozturk90@gmail.com>
2013-10-14 17:47:00 +0300

    feature spec for inviting users to a team

---
+++ spec/features/team_leader_invites_users_or_translators_to_the_team_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature 'team leader invites users or translators to the team' do 
+  background do 
+    @user2 = create(:user)
+    create_user_and_login
+    visit user_path(@user)
+    click_link 'Grup Oluştur'
+    fill_in 'Grup İsmi', with: 'Decoy Group'
+    click_button 'Grubu Kur' 
+    click_link 'Gruba Ekle'
+    click_button 'Gruba Davet Et'
+  end
+
+  scenario 'they recieve a request to join the team' do 
+    click_link 'cikis yap'
+    visit new_session_path
+    fill_in 'Email', with: @user2.email
+    fill_in 'Şifre', with: @user2.password
+    click_button 'Giriş Yap'
+    visit user_path(@user2)
+    click_link 'Katılım İsteği'
+    click_button 'Kabul Et'
+    expect(page).to have_content 'Çeviri Yaptır'
+    expect(page).to have_content 'Grup Üyeleri'
+    expect(page).to have_content 'Decoy Group'
+  end
+end
