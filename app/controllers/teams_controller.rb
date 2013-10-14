a8446d342b2546d920d43218c6cb7339fef5a02e
Burak Öztürk <burakozturk90@gmail.com>
2013-10-14 10:40:53 +0300

    Rewrite the flash messages to be more descriptive

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,9 +8,9 @@ class TeamsController < ApplicationController
     @team.leader_id = current_user.id
     if @team.save
       current_user.update_columns(leader: 'true', leader_of_team: @team.id)
-      redirect_to user_path(current_user), success: 'Basarili'
+      redirect_to user_path(current_user), success: 'Grup kuruldu...Grup işlemleri sekmesinden grubunuzu yönetebilirsiniz.'
     else
-      redirect_to new_user_teams_path, danger: 'birdaha dene'
+      redirect_to new_user_teams_path, danger: 'Grup oluştururken bir hata oluştu.Tekrara denemenizde fayda var.'
     end
   end
   
