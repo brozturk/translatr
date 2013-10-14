56bd74ae2f8aa32ed81c40d300d4613a87094bff
Burak Öztürk <burakozturk90@gmail.com>
2013-10-14 17:46:39 +0300

    create user_teams resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Translatr::Application.routes.draw do
   resources :users 
   resources :sessions, only: [:create, :destroy, :new]
   resources :teams
+  resources :user_teams
   root 'sessions#new'
   match 'signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation: first created -> highest priority.
