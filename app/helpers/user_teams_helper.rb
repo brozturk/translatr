693d290cdf239bab4add1905bc304e900ad91fde
Burak Öztürk <burakozturk90@gmail.com>
2013-10-15 14:28:57 +0300

    add the request methods that will be used in the general nav

--- app/helpers/user_teams_helper.rb
+++ app/helpers/user_teams_helper.rb
@@ -1,2 +1,18 @@
 module UserTeamsHelper
+  def group_invite_request
+    UserTeam.where(user_id: current_user.id, state: 'requested')
+  end
+
+  def request_size
+    group_invite_request.size
+  end
+
+  def user_has_requests?
+    if group_invite_request.size == 0 
+      return false
+    else
+      return true
+    end
+  end
+
 end
