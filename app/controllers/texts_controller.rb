998c7539eba4e05ff6d59a9017d0034a38c4b344
Burak Öztürk <burakozturk90@gmail.com>
2013-10-17 20:01:41 +0300

    controller create action

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -5,7 +5,18 @@ class TextsController < ApplicationController
   end
 
   def create
+    @text = Text.create(text_params)
+    if @text.save
+      redirect_to new_text_path,  success: 'Çeviri başarılı şekilde kaydedildi.'
+    else
+      redirect_to new_text_path, danger: 'Çeviri oluşturulması sırasında bir hata oluştu lütfen tekrar deneyin.' 
+    end
+  end
 
+  private
+
+  def text_params
+    params.require(:text).permit(:title, :text)
   end
 
 end
