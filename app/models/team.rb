c1794707187108ecfc6ea6f274aebf68eeb2e643
Burak Öztürk <burakozturk90@gmail.com>
2013-10-17 17:08:48 +0300

    fix lines where a quesry would look for the state column in users instead of user_teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,12 +3,12 @@ class Team < ActiveRecord::Base
   
   has_many :user_teams
 
-  has_many :users, -> { where status: 'accepted' }, through: :user_teams
+  has_many :users, -> { where user_teams: { state: 'accepted' } }, through: :user_teams
 
-  has_many :requested_user_teams, -> { where status: 'requested' }, foreign_key: :team_id, class_name: 'UserTeam'
+  has_many :requested_user_teams, -> { where user_teams: { state: 'requested' } }, foreign_key: :team_id, class_name: 'UserTeam'
   has_many :requested_users, through: :requested_user_teams, source: :user
 
-  has_many :kicked_user_teams, -> { where status: 'kicked' }, foreign_key: :team_id, class_name: 'UserTeam'
+  has_many :kicked_user_teams, -> { where user_teams: { state: 'kicked' } }, foreign_key: :team_id, class_name: 'UserTeam'
   has_many :kicked_users, through: :kicked_user_teams, source: :user
   
   has_many :texts, through: :users
