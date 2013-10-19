fb7c25150a1b320368d73b98aa8658c90469ad3f
Burak Öztürk <burakozturk90@gmail.com>
2013-10-19 19:53:10 +0300

    update specs to add belongs_to relationship to teams

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Text do 
-  it {should belong_to (:user)}
-  it {should have_one(:translation)}
-  it {should validate_presence_of(:title)} 
-  it {should validate_presence_of(:text)} 
-  it {should validate_presence_of(:user_id)} 
+  it { should belong_to(:user) }
+  it { should belong_to(:team) }
+  it { should have_one(:translation) }
+  it { should validate_presence_of(:title) } 
+  it { should validate_presence_of(:text) } 
+  it { should validate_presence_of(:user_id) } 
 end
--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -7,4 +7,5 @@ describe Translation do
   it { should validate_presence_of(:text) } 
   it { should belong_to(:user) } 
   it { should belong_to(:text) }
+  it { should belong_to(:team) } 
 end
