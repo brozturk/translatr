0bdc50f16dc7a1fab6d0f4ee70d764f679d5c2f9
Burak Öztürk <burakozturk90@gmail.com>
2013-10-20 12:08:28 +0300

    set team leader as an accepted user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,9 +8,10 @@ class TeamsController < ApplicationController
     @team.leader_id = current_user.id
     if @team.save
       current_user.update_columns(leader: 'true', leader_of_team: @team.id)
+      UserTeam.create(user_id: current_user.id, team_id: @team.id, state: 'accepted')
       redirect_to user_path(current_user), success: 'Grup kuruldu...Grup işlemleri sekmesinden grubunuzu yönetebilirsiniz.'
     else
-      redirect_to new_user_teams_path, danger: 'Grup oluştururken bir hata oluştu.Tekrara denemenizde fayda var.'
+      redirect_to new_team_path, danger: 'Grup oluştururken bir hata oluştu.Tekrara denemenizde fayda var.'
     end
   end
   
