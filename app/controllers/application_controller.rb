4260f942c406475018aa6c2fc6fd3805637a5416
Burak Öztürk <burakozturk90@gmail.com>
2013-10-25 17:55:20 +0300

    initial steps for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
   include UserTeamsHelper
 
-  
   add_flash_types :success, :danger 
+
+  private
+
+  def current_permission
+    @current_permission ||= Permission.new(current_user)
+  end
+
+  def authorize
+    if !current_permission.allow?(params[:controller], params[:action])
+      redirect_to root_url, danger: 'Önce Giriş Yapmalısınız'
+    end
+  end
   
 end
