ba5b1fb513dbb352a96e8101740377f05e2935b5
Burak Öztürk <burakozturk90@gmail.com>
2013-10-25 23:00:58 +0300

    custom rspec matcher for testing controllers and actionsfor authorization

--- spec/support/matchers/allow.rb
+++ spec/support/matchers/allow.rb
@@ -1,5 +1,17 @@
-RSpec::Matchers.define :allow do |*args| 
+RSpec::Matchers.define :allow_action do |*args|
   match do |permission|
-    permisssion.allow?(*args).should be_true
+    permission.allow?(*args).should be_true
+  end
+
+  failure_message_for_should do |permission|
+    "expected to have permission for these actions"
+  end
+
+  failure_message_for_should_not do |permission|
+    "expected to not have permission for these actions"
+  end
+
+  description do 
+    "allow access to the %{*args} actions"
   end
 end
