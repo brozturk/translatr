7f032616dd9d8edf146115c2b2fcce62b3f2aa71
Burak Öztürk <burakozturk90@gmail.com>
2013-10-26 19:43:18 +0300

    permissin model for authorization

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,9 +2,30 @@ class Permission < Struct.new(:user)
   def allow?(controller, action)
     return true if controller == 'sessions'
     return true if controller == 'users' && action.in?(%w[new create])
-    if user
-    return true if controller == 'users' && action.in?(%w[index show edit update])
-    return true if controller == 'teams' && action.in?(%w[create index])
+
+    if user && user.teams.count == 0 && !user.translator
+      return true if controller == 'users' && action.in?(%w[index show edit update])
+      return true if controller == 'teams' && action.in?(%w[new create index])
+      return true if controller == 'user_teams' && action.in?(%w[create index update])
+
+    elsif !user.leader && !user.translator && user.teams.count > 0
+      return true if controller == 'users' && action.in?(%w[index show edit update])
+      return true if controller == 'teams' && action.in?(%w[show new create index])
+      return true if controller == 'user_teams' && action.in?(%w[create index update])
+      return true if controller == 'texts' && action.in?(%w[show new create index])
+
+    elsif  user.teams.count > 0 && !user.translator && user.leader 
+      return true if controller == 'users' && action.in?(%w[index show edit update])
+      return true if controller == 'teams' && action.in?(%w[show new create index update edit destroy])
+      return true if controller == 'user_teams' && action.in?(%w[create index update])
+      return true if controller == 'texts' && action.in?(%w[show new create index])
+
+    elsif user.translator 
+      return true if controller == 'users' && action.in?(%w[index show edit update])
+      return true if controller == 'teams' && action.in?(%w[show new create index])
+      return true if controller == 'user_teams' && action.in?(%w[create index update])
+      return true if controller == 'texts' && action.in?(%w[show index])
+      return true if controller == 'translations' && action.in?(%w[create])
     end
   end
 end
