5dc43428a01c4b41ceb9415f64ab116a3022f23d
Burak Öztürk <burakozturk90@gmail.com>
2013-10-28 15:30:26 +0200

    prevent users with a team from accessing other teams pages

--- spec/features/user_restrictions_spec.rb
+++ spec/features/user_restrictions_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 feature 'user is restricted' do
   background do 
-      @user = create(:user)
-      @other_user = create(:user)
-      @team = create(:team)
-      relationship = create(:user_team, user_id: @user.id, team_id: @team.id, state: 'accepted')
-      relationship2 = create(:user_team, user_id: @other_user.id, team_id: @team.id, state: 'accepted')
-      @users_text = create(:text, user_id: @user.id, team_id: @team.id)
-      @other_text = create(:text, user_id: @other_user.id,  team_id: @team.id)
+    @user = create(:user)
+    @other_user = create(:user)
+    @team = create(:team)
+    @other_team = create(:team)
+    relationship = create(:user_team, user_id: @user.id, team_id: @team.id, state: 'accepted')
+    relationship2 = create(:user_team, user_id: @other_user.id, team_id: @team.id, state: 'accepted')
+    @users_text = create(:text, user_id: @user.id, team_id: @team.id)
+    @other_text = create(:text, user_id: @other_user.id,  team_id: @team.id)
   end
   scenario 'when deleting texts except the ones they created' do 
    visit new_session_path
@@ -42,5 +43,17 @@ feature 'user is restricted' do
    fill_in 'Yeni yazıyı buraya girin', with: 'edited text'
    click_button 'Tamamdır'
    expect(page).to have_content 'edited text'
+  end
+  
+  scenario 'when trying to visit the page of a team they do not belong to' do 
+   visit new_session_path
+   fill_in 'Mail Adresi', with: @user.email
+   fill_in 'Şifre', with: @user.password
+   click_button 'Giriş'
+   visit team_texts_path(@other_team)
+   expect(page).to have_content 'Bunu yapmaya izniniz yok'
+   visit team_texts_path(@team)
+   expect(page).not_to have_content 'Bunu yapmaya izniniz yok'
+   expect(current_path).to eq team_texts_path(@team)
   end
 end
