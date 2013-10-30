490951aca7e3a28dea6900602475c38d843f5a25
Burak Öztürk <burakozturk90@gmail.com>
2013-10-30 18:08:20 +0200

    remode dependent: :destroy from the translation model and apply it to the text model

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,7 +1,7 @@
 class Text < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
-  has_one :translation
+  has_one :translation, dependent: :destroy
 
   validates :title, presence: true
   validates :text, presence: true
