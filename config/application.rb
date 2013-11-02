803d9bb74771cfcd6f32ec945b6631ed94d0b8c9
Burak Öztürk <burakozturk90@gmail.com>
2013-11-02 23:55:24 +0200

    configure the application to send emails through gmail

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Translatr
 
     #set domain for sending emails
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'translatr.herokuapp.com',
+      user_name:            ENV["GMAIL_USERNAME"], 
+      password:             ENV["GMAIL_PASSWORD"], 
+      authentication:       'plain',
+      enable_starttls_auto: true  }
   end
 end
