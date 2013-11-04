887654f7faf2ad247e3e0f2c21a14c0c682d61c2
Burak Öztürk <burakozturk90@gmail.com>
2013-11-04 20:18:46 +0200

    fix the page that displays team requests to use a table and properly display names of the team leader and team

--- app/helpers/user_teams_helper.rb
+++ app/helpers/user_teams_helper.rb
@@ -3,6 +3,14 @@ module UserTeamsHelper
     UserTeam.where(user_id: current_user.id, state: 'requested')
   end
 
+  def group_invite_request_team_leader(request)
+    User.find(request.team.leader_id)
+  end
+
+  def name_of_the_team_leader_who_invited(request)
+    User.find(request.team.leader_id).name + User.find(request.team.leader_id).last_name
+  end
+
   def request_size
     group_invite_request.size
   end
