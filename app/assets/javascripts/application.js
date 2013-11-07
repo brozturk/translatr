dbe04c1c577c06be683e9dc20499128ccbeb7f10
Burak Öztürk <burakozturk90@gmail.com>
2013-11-07 21:17:55 +0200

    applicaiton.js fix for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
 //= require initial
 //= require turbolinks 
 //= require faq
